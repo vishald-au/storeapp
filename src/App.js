212ff61c5beee8ef53af0cebc9a608d4220f4956
Vishal Dhingra <dhingra.vishal1@gmail.com>
2021-07-07 10:43:00 +0930

    working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Box, Container, Grid, Paper } from '@material-ui/core';
+import Sidebar from './Sidebar'
 
 function App() {
   return (
-    <Container maxWidth="sm" className='borderArea'>
+    <Container maxWidth="lg" className='borderArea'>
         <Box className='frame'>
           <Grid container>
           <Grid item xs={4}>
-              <Paper className='equalHeight navArea' elevation={0} square>TEST</Paper>
+                <Sidebar />
             </Grid>
             <Grid item xs={8}>
               <Paper className='equalHeight' elevation={0} square>TEST</Paper>
---
+++ src/Sidebar.js
@@ -0,0 +1,20 @@
+import {Drawer} from '@material-ui/core'
+
+const Sidebar = () => {
+    return (
+      <div className='changePosition'>
+           <Drawer variant="permanent" anchor="left" className='changeDrawer'>
+                <div>Sidebar</div>
+                <div><p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sit amet massa at odio commodo sagittis id sit amet arcu. Quisque vestibulum nunc vel elit vulputate egestas at in arcu. Praesent luctus mi sodales turpis viverra placerat. Vestibulum congue ex sed bibendum tempor. Aliquam ultricies risus sit amet ligula iaculis, vitae tincidunt lectus placerat. Etiam turpis velit, blandit sit amet ullamcorper vel, molestie sed dolor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque laoreet ex a auctor semper. Pellentesque dolor eros, dignissim at vestibulum quis, varius at ipsum. Nunc at iaculis magna, aliquet rhoncus erat. Phasellus imperdiet, leo in malesuada suscipit, lacus lorem auctor leo, in congue massa massa vel risus.
+<br/>
+<br/>
+Vestibulum nec mi elit. Sed iaculis lorem mi. Sed porttitor arcu ut pharetra rutrum. Phasellus congue vitae enim sit amet egestas. Nam luctus ligula auctor ultricies mattis. Pellentesque ipsum odio, molestie eget tortor at, bibendum aliquet ipsum. Cras in elit in arcu volutpat aliquam. Proin tempor massa eget dapibus pulvinar. Quisque accumsan commodo tellus, eget tempor dui dignissim vitae. Curabitur nec velit vitae nisl consectetur scelerisque quis quis felis. Pellentesque sodales lorem vitae lacinia gravida. Vivamus dolor nisl, posuere vel varius eget, posuere eu dui. Donec auctor sem sit amet purus viverra convallis. Pellentesque quis quam velit. Etiam elit ex, dignissim at eros quis, bibendum porttitor velit.
+
+</p></div>
+            </Drawer>
+            </div>
+    )
+}
+
+export default Sidebar
