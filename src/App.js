9c94fac430ed3daec06045ecc6aca25d045ab781
Vishal Dhingra <dhingra.vishal1@gmail.com>
2021-07-07 10:04:51 +0930

    working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Container, Grid, Paper } from '@material-ui/core';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm" className='borderArea'>
+        <Box className='frame'>
+          <Grid container>
+          <Grid item xs={4}>
+              <Paper className='equalHeight navArea' elevation={0} square>TEST</Paper>
+            </Grid>
+            <Grid item xs={8}>
+              <Paper className='equalHeight' elevation={0} square>TEST</Paper>
+            </Grid>
+          </Grid>
+        </Box>
+    </Container>
   );
 }
 
